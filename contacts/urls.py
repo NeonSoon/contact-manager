0b2ffecf8df8ff1903900d58e5361ab11029e5e7
NeonSoon <gp152333896@gmail.com>
2025-08-13 22:08:21 +0800

    成功讓網頁顯示預設的句子

--- contacts/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('contacts/', views.contact_list, name='contact_list'),
-]
--- contacts/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def contact_list(request):
-    return HttpResponse("這是聯絡人列表頁面")
