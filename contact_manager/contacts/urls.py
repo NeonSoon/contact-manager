efa11c670269e4cd60bec245b1eec6f04150e9cf
NeonSoon <gp152333896@gmail.com>
2025-08-18 11:29:10 +0800

    完成C-R-U(update)

--- contact_manager/contacts/urls.py
+++ contact_manager/contacts/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('contacts/', views.contact_list, name='contact_list'),
+    path('contacts/add/', views.add_contact, name='add_contact'),
+    path('contacts/edit/<int:contact_id>/', views.edit_contact, name='edit_contact'),
 ]
--- contact_manager/contacts/views.py
+++ contact_manager/contacts/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Contact
+from .forms import ContactForm
+from django.shortcuts import get_object_or_404
+
 
 def contact_list(request):
     contacts = Contact.objects.all()
     return render(request, "contacts/contact_list.html", {"contacts": contacts})
+
+def add_contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('contact_list')  # 新增後回到列表頁
+    else:
+        form = ContactForm()
+    return render(request, 'contacts/add_contact.html', {'form': form})
+
+def edit_contact(request, contact_id):
+    contact = get_object_or_404(Contact, id=contact_id)
+    if request.method == "POST":
+        form = ContactForm(request.POST, instance=contact)
+        if form.is_valid():
+            form.save()
+            return redirect("contact_list")
+    else:
+        form = ContactForm(instance=contact)
+    return render(request, "contacts/edit_contact.html", {"form": form, "contact": contact})
